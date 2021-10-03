114b313713bdbdeaba088d015ab73942e3a123a2
Ericia <89178727+ericiachan@users.noreply.github.com>
2021-10-03 21:22:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(8080, () => {
+ console.log("Server running on port 8080");
 });
 
 app.get("/", (req, res) => {
