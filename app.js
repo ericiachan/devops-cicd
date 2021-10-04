60fbac32fb86a368c68436c9107ccfe0c060cd88
Ericia <89178727+ericiachan@users.noreply.github.com>
2021-10-04 19:04:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,26 +8,6 @@ app.get("/", (req, res) => {
  res.send("Endpoint is live - deployment success!");
 });
 
-app.get("/api", (req, res, next) => {
- res.json(["Lorem","Ipsum","Dolor","Sit","Amet"]);
-});
-
-app.get("/fx-static", (req, res, next) => {
- res.json({"sell": "589.181", "timestamp": "2020-06-17 14:42:34.492974", "buy": "489.1"});
-});
-
-app.get("/fx", (req, res, next) => {
- rand1 = Math.floor(Math.random() * 90) + 10;
- rand2 = Math.floor(Math.random() * 90) + 10;
-
- var today = new Date();
- var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
- var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
- var dateTime = date+' '+time;
-
- res.json({"sell": "387." + rand1, "timestamp": dateTime, "buy": "287." + rand2});
-});
-
 app.get("/random", (req, res, next) => {
  res.json([Math.floor(Math.random() * 90) + 10]);
 });
